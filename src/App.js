3d37b6cd14a72a9be56d276f66a6c88c6464798c
UMKC CTCFP User <umkc-ctcfpuser@umkc.edu>
2018-06-07 00:03:16 -0500

    added waterfall images

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import waterfall from "./waterfall.json";
+import "./App.css";
 
 class App extends Component {
+  // Setting this.state.waterfall to the waterfall json array
+  state = {
+    waterfall
+  };
+
+  removewaterfall = id => {
+    // Filter this.state.waterfall for waterfall with an id not equal to the id being removed
+    const waterfall = this.state.waterfall.filter(waterfall => waterfall.id !== id);
+    // Set this.state.waterfall equal to the new waterfall array
+    this.setState({ waterfall });
+  };
+
+  // Map over this.state.waterfall and render a waterfallCard component for each waterfall object
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Wrapper>
+        <Title>waterfall List</Title>
+        {this.state.waterfall.map(waterfall => (
+          <FriendCard
+            removewaterfall={this.removewaterfall}
+            id={waterfall.id}
+            key={waterfall.id}
+            name={waterfall.name}
+            image={waterfall.image}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
